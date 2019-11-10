f36af148418b17fd60a90d3e88e61e15a4493eaf
Kevin Michael <biobot.mike@gmail.com>
2019-11-10 14:55:24 +0200

    Add revealing module pattern example

--- module/app.js
+++ module/app.js
@@ -27,3 +27,28 @@
 // })();
 
 // UICtrl.callChangeText();
+
+// REVEALING MODULE PATTERN
+const ItemCtrl = (function() {
+  let data = [];
+
+  function add(item) {
+    data.push(item);
+    console.log('Item Added....');
+  }
+
+  function get(id) {
+    return data.find(item => {
+      return item.id === id;
+    });
+  }
+
+  return {
+    add: add,
+    get: get
+  }
+})();
+
+ItemCtrl.add({id: 1, name: 'John'});
+ItemCtrl.add({id: 2, name: 'Mary'});
+console.log(ItemCtrl.get(2));
